032ab60985cd505e9ace2c55ade776745c4bf63b
MorKaragh <hentai.exe@gmail.com>
2019-10-28 22:32:37 +0300

    math is strange

--- fractal.py
+++ fractal.py
@@ -1,15 +1,21 @@
 import pygame
 from random import randrange as rnd
 
+DIMENSIONS = 3
+
+SIZE = 1000
+
+DIVISION_FACTOR = 2
+
 
 def run_fractal(dimensions):
     pygame.init()
     pygame.display.set_caption("chaotic beauty")
-    screen = pygame.display.set_mode((1000, 1000))
+    screen = pygame.display.set_mode((SIZE, SIZE))
 
     points = []
     for i in range(0, dimensions + 1):
-        points.append([rnd(1, 1000), rnd(1, 1000)])
+        points.append([rnd(1, SIZE), rnd(1, SIZE)])
 
     calculated_points = []
 
@@ -33,11 +39,11 @@ def run_fractal(dimensions):
 
 def get_middle_point(acting_point, destination_point):
     x_c = int(min(acting_point[0], destination_point[0]) + (
-            max(acting_point[0], destination_point[0]) - min(acting_point[0], destination_point[0]))/2)
+            max(acting_point[0], destination_point[0]) - min(acting_point[0], destination_point[0])) / DIVISION_FACTOR)
     y_c = int(min(acting_point[1], destination_point[1]) + (
-            max(acting_point[1], destination_point[1]) - min(acting_point[1], destination_point[1]))/2)
+            max(acting_point[1], destination_point[1]) - min(acting_point[1], destination_point[1])) / DIVISION_FACTOR)
     move_to = [x_c, y_c]
     return move_to
 
 
-run_fractal(5)
+run_fractal(DIMENSIONS)
